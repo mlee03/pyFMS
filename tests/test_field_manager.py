053903e6abb2a0a480ed618247b5a1ad13fd9fa5
Frank Malatino <frank.malatino@noaa.gov>
2025-01-08 10:57:36 -0500

    Added additional module to test config for FieldTable unit tests

--- tests/test_field_manager.py
+++ tests/test_field_manager.py
@@ -40,7 +40,35 @@ test_config = {
                             ],
                         },
                     ],
-                }
+                },
+                {
+                    "model_type": "other_mod",
+                    "varlist": [
+                        {
+                            "variable": "test_var_name",
+                            "longname": "test_variable_name",
+                            "units": "t/t",
+                            "profile_type": [
+                                {"value": "fixed", "surface_value": 3e-06}
+                            ],
+                        },
+                        {
+                            "variable": "soc",
+                            "longname": "SOC tracer",
+                            "units": "mr",
+                            "convection": "all",
+                            "chem_params": [
+                                {
+                                    "value": "aerosol",
+                                    "frac_pm2": 0.89,
+                                    "frac_pm26": 0.96,
+                                    "frac_pm11": 1.0,
+                                }
+                            ],
+                            "profile_type": [{"value": "flex", "surface_value": 1e-33}],
+                        },
+                    ],
+                },
             ],
         }
     ]
@@ -282,3 +310,97 @@ def test_set_subparam_name_duplicate(temp_file):
         oldname="frac_pm1",
         newname="frac_pm1",
     )
+
+
+def test_access_other_module(temp_file):
+    test_table = FieldTable.from_file(temp_file)
+
+    new_var = {
+        "variable": "test",
+        "longname": "longtest",
+        "units": "m",
+        "profile_type": "fixed",
+        "subparams": [{"surface_value": 1}],
+    }
+    test_table.add_to_varlist(module="other_mod", var=new_var)
+    varlist = [mod for mod in test_table.modlist if mod["model_type"] == "other_mod"][
+        0
+    ]["varlist"]
+    assert new_var in varlist
+
+    var = test_table.get_var(module="other_mod", varname="soc")
+    assert var == test_table.modlist[1]["varlist"][1]
+
+    subparameter = test_table.get_subparam(
+        module="other_mod", varname="soc", subparam_name="chem_params"
+    )
+    assert subparameter == test_table.modlist[1]["varlist"][1]["chem_params"]
+
+    value = test_table.get_value(
+        module="other_mod", varname="test_var_name", key="units"
+    )
+    assert value == "t/t"
+
+    value = test_table.get_subparam_value(
+        module="other_mod",
+        varname="soc",
+        listname="chem_params",
+        paramname="frac_pm2",
+    )
+    assert value == 0.89
+
+    test_list = test_table.get_variable_list(module="other_mod")
+    assert test_list == ["test_var_name", "soc", "test"]
+
+    num_vars = test_table.get_num_variables(module="other_mod")
+    assert num_vars == 3
+
+    test_list = test_table.get_subparam_list(module="other_mod", varname="test")
+    assert test_list == ["subparams"]
+
+    num = test_table.get_num_subparam(module="other_mod", varname="test")
+    assert num == 1
+
+    test_table.set_value(module="other_mod", varname="soc", key="units", value="m")
+    varlist = [mod for mod in test_table.modlist if mod["model_type"] == "other_mod"][
+        0
+    ]["varlist"]
+    assert varlist[1]["units"] == "m"
+
+    test_table.set_subparam_value(
+        module="other_mod",
+        varname="soc",
+        listname="chem_params",
+        subparamname="frac_pm2",
+        value=1,
+    )
+    varlist = [mod for mod in test_table.modlist if mod["model_type"] == "other_mod"][
+        0
+    ]["varlist"]
+    assert varlist[1]["chem_params"][0]["frac_pm2"] == 1
+
+    test_table.set_var_name(module="other_mod", old_name="soc", new_name="sob")
+    varlist = [mod for mod in test_table.modlist if mod["model_type"] == "other_mod"][
+        0
+    ]["varlist"]
+    assert varlist[1]["variable"] == "sob"
+
+    test_table.set_var_attr_name(
+        module="other_mod", varname="sob", oldname="longname", newname="ln"
+    )
+    varlist = [mod for mod in test_table.modlist if mod["model_type"] == "other_mod"][
+        0
+    ]["varlist"]
+    assert varlist[1]["ln"]
+
+    test_table.set_subparam_name(
+        module="other_mod",
+        varname="sob",
+        listname="chem_params",
+        oldname="frac_pm2",
+        newname="frac_pm1",
+    )
+    varlist = [mod for mod in test_table.modlist if mod["model_type"] == "other_mod"][
+        0
+    ]["varlist"]
+    assert varlist[1]["chem_params"][0]["frac_pm1"]
