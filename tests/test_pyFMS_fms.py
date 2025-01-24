03966d2c39980b7e7466dfcf3cec0474ebdc488a
Frank Malatino <frank.malatino@noaa.gov>
2025-01-24 14:55:15 -0500

    Changed name of unit test for field manager

--- tests/test_pyFMS_fms.py
+++ tests/test_pyFMS_fms.py
@@ -5,13 +5,6 @@ from mpi4py import MPI
 from pyfms import pyFMS
 
 
-# @pytest.fixture(scope="session")
-# def temp_file(tmpdir_factory):
-#     file_path = tmpdir_factory.mktemp("data").join("input.nml")
-#     yield file_path
-#     os.remove(file_path)
-
-
 def test_pyfms_init():
     assert os.path.exists("./cFMS/libcFMS/.libs/libcFMS.so")
 
@@ -22,6 +15,6 @@ def test_pyfms_init():
         clibFMS_path="./cFMS/libcFMS/.libs/libcFMS.so",
     )
 
-    # assert isinstance(pyfmsobj, pyFMS)
+    assert isinstance(pyfmsobj, pyFMS)
 
     pyfmsobj.pyfms_end()
