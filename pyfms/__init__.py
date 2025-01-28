226b160d6c8f195d4d0e16614d885d4a50fa30a2
Frank Malatino <frank.malatino@noaa.gov>
2025-01-28 14:57:06 -0500

    Amended data_handling

--- pyfms/__init__.py
+++ pyfms/__init__.py
@@ -1,3 +1,5 @@
 from .pyFMS_error import pyfms_error
 from .pyFMS_field_manager import FieldError, FieldTable
 from .pyFMS_fms import pyFMS
+from .mpp.pyFMS_mpp import pyFMS_mpp
+from .mpp.pyFMS_mpp_domains import pyFMS_mpp_domains
--- tests/test_pyFMS_fms.py
+++ tests/test_pyFMS_fms.py
@@ -1,6 +1,7 @@
 import os
 
 from mpi4py import MPI
+import ctypes as ct
 
 from pyfms import pyFMS
 
@@ -17,4 +18,12 @@ def test_pyfms_init():
 
     assert isinstance(pyfmsobj, pyFMS)
 
+    pyfmsobj.pyfms_set_pelist_npes(2)
+
+    npes = ct.c_int.in_dll(pyfmsobj.clibFMS, "cFMS_pelist_npes")
+
+    assert npes.value == 2
+
     pyfmsobj.pyfms_end()
+
+
