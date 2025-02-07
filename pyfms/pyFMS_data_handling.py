91069c24229a25337dd2dce9b9719c118160b02d
Frank Malatino <frank.malatino@noaa.gov>
2025-02-07 08:26:05 -0500

    Using full name for ctypes, full address for cFMS, relative path of hared object for pyfms_init

--- pyfms/pyFMS_data_handling.py
+++ pyfms/pyFMS_data_handling.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import ctypes as c
+import ctypes
 
 import numpy as np
 
@@ -11,72 +11,74 @@ def set_ndpointer(arg, c_type):
 
 def setarray_Cint32(arg):
     if arg[0] is None:
-        return None, c.POINTER(c.c_int)
+        return None, ctypes.POINTER(ctypes.c_int)
     else:
-        return arg, set_ndpointer(arg, c.c_int)
+        return arg, set_ndpointer(arg, ctypes.c_int)
 
 
 def setarray_Cfloat(arg):
     if arg[0] is None:
-        return None, c.POINTER(c.c_float32)
+        return None, ctypes.POINTER(ctypes.c_float32)
     else:
-        return arg, set_ndpointer(arg, c.c_float)
+        return arg, set_ndpointer(arg, ctypes.c_float)
 
 
 def setarray_Cdouble(arg):
     if arg[0] is None:
-        return None, c.POINTER(c.c_double)
+        return None, ctypes.POINTER(ctypes.c_double)
     else:
-        return arg, set_ndpointer(arg, c.c_double)
+        return arg, set_ndpointer(arg, ctypes.c_double)
 
 
 def set_Cchar(arg):
     if arg is None:
-        return arg, c.POINTER(c.c_char_p)
+        return arg, ctypes.POINTER(ctypes.c_char_p)
     else:
-        return c.byref(c.c_char_p(arg.encode("ascii"))), c.POINTER(c.c_char_p)
+        return ctypes.byref(ctypes.c_char_p(arg.encode("ascii"))), ctypes.POINTER(
+            ctypes.c_char_p
+        )
 
 
 def setarray_Cbool(arg):
     if arg[0] is None:
-        return None, c.POINTER(c.c_bool)
+        return None, ctypes.POINTER(ctypes.c_bool)
     else:
-        return arg, set_ndpointer(arg, c.c_bool)
+        return arg, set_ndpointer(arg, ctypes.c_bool)
 
 
 def setscalar_Cint32(arg):
     if arg is None:
-        return arg, c.POINTER(c.c_int)
+        return arg, ctypes.POINTER(ctypes.c_int)
     else:
-        return c.byref(c.c_int(arg)), c.POINTER(c.c_int)
+        return ctypes.byref(ctypes.c_int(arg)), ctypes.POINTER(ctypes.c_int)
 
 
 def setscalar_Cfloat(arg):
     if arg is None:
-        return arg, c.POINTER(c.c_float)
+        return arg, ctypes.POINTER(ctypes.c_float)
     else:
-        return c.byref(c.c_float(arg)), c.POINTER(c.c_float)
+        return ctypes.byref(ctypes.c_float(arg)), ctypes.POINTER(ctypes.c_float)
 
 
 def setscalar_Cdouble(arg):
     if arg is None:
-        return arg, c.POINTER(c.c_double)
+        return arg, ctypes.POINTER(ctypes.c_double)
     else:
-        return c.byref(c.c_double(arg)), c.POINTER(c.c_double)
+        return ctypes.byref(ctypes.c_double(arg)), ctypes.POINTER(ctypes.c_double)
 
 
 def setscalar_Cbool(arg):
     if arg is None:
-        return arg, c.POINTER(c.c_double)
+        return arg, ctypes.POINTER(ctypes.c_double)
     else:
-        return c.byref(c.c_bool(arg)), c.POINTER(c.c_bool)
+        return ctypes.byref(ctypes.c_bool(arg)), ctypes.POINTER(ctypes.c_bool)
 
 
 def set_sizevars(arg, ndim):
     if arg[0] is None:
-        argsize = [c.byref(c.c_int(1)) for i in range(ndim)]
-        argsize_t = [c.POINTER(c.c_int) for i in range(ndim)]
+        argsize = [ctypes.byref(ctypes.c_int(1)) for i in range(ndim)]
+        argsize_t = [ctypes.POINTER(ctypes.c_int) for i in range(ndim)]
     else:
-        argsize = [c.byref(c.c_int(isize)) for isize in np.shape(arg)]
-        argsize_t = [c.POINTER(c.c_int) for i in range(ndim)]
+        argsize = [ctypes.byref(ctypes.c_int(isize)) for isize in np.shape(arg)]
+        argsize_t = [ctypes.POINTER(ctypes.c_int) for i in range(ndim)]
     return argsize, argsize_t
