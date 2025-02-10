ffd44a9ce6b01fad557d14f3cc18bd3199bf1601
Frank Malatino <frank.malatino@noaa.gov>
2025-02-10 14:15:12 -0500

    Adding mpp unit tests to workflow

--- tests/test_pyFMS_fms.py
+++ tests/test_pyFMS_fms.py
@@ -1,7 +1,4 @@
-import ctypes as ct
 import os
-
-from mpi4py import MPI
 
 from pyfms import pyFMS
 
@@ -9,20 +6,8 @@ from pyfms import pyFMS
 def test_pyfms_init():
     assert os.path.exists("./cFMS/libcFMS/.libs/libcFMS.so")
 
-    fcomm = MPI.COMM_WORLD.py2f()
-
-    pyfmsobj = pyFMS(
-        localcomm=fcomm,
-        clibFMS_path="./cFMS/libcFMS/.libs/libcFMS.so",
-        alt_input_nml_path="input.nml",
-    )
+    pyfmsobj = pyFMS(clibFMS_path="./cFMS/libcFMS/.libs/libcFMS.so")
 
     assert isinstance(pyfmsobj, pyFMS)
-
-    pyfmsobj.set_pelist_npes(2)
-
-    npes = ct.c_int.in_dll(pyfmsobj.clibFMS, "cFMS_pelist_npes")
-
-    assert npes.value == 2
 
     pyfmsobj.pyfms_end()
