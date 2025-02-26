d4da5483ad234e4018519a445557426fced31bfd
Frank Malatino <frank.malatino@noaa.gov>
2025-02-26 15:17:28 -0500

    Forgot to remove domain objects from __init__

--- pyfms/__init__.py
+++ pyfms/__init__.py
@@ -1,4 +1,3 @@
-from .mpp.pyFMS_domain_obj import Domain, NestDomain
 from .mpp.pyFMS_mpp import pyFMS_mpp
 from .mpp.pyFMS_mpp_domains import pyFMS_mpp_domains
 from .pyFMS_field_manager import FieldTable
