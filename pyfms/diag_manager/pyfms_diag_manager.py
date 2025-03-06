dea232f6c165dde5afe034fb5b250061ae912870
Frank Malatino <frank.malatino@noaa.gov>
2025-03-06 14:27:43 -0500

    Added public level diag_manager methods as of 14:27 6 March 2025

--- pyfms/diag_manager/pyfms_diag_manager.py
+++ pyfms/diag_manager/pyfms_diag_manager.py
@@ -47,27 +47,168 @@ class pyFMS_diag_manager:
 
         return err_msg_c.value.decode('utf-8')
     
+    def diag_send_complete(
+            self,
+            diag_field_id: int,
+    ) -> str:
+        err_msg = ""
+
+        _cfms_diag_send_complete = self.clibFMS.CFMS_diag_send_complete
+
+        diag_field_id_c, diag_field_id_t = setscalar_Cint32(diag_field_id)
+        err_msg_c, err_msg_t = set_Cchar(err_msg)
+
+        _cfms_diag_send_complete.argtypes = [
+            diag_field_id_t,
+            err_msg_t
+        ]
+        _cfms_diag_send_complete.restype = None
+
+        _cfms_diag_send_complete(diag_field_id_c, err_msg_c)
+
+        return err_msg_c.value.decode('utf-8')
+    
     def diag_set_field_init_time(
-        self,
-        seconds: Optional[int] = None,
-        days: Optional[int] = None,
-        ticks: Optional[int] = None,
-    ):
+            self,
+            year: int,
+            month: int,
+            day: int,
+            hour: int,
+            minute: int,
+            second: Optional[int],
+            tick: Optional[int],
+    ) -> str:
+        err_msg = ""
+
         _cfms_diag_set_field_init_time = self.clibFMS.cFMS_diag_set_field_init_time
 
-        seconds_c, seconds_t = setscalar_Cint32(seconds)
-        days_c, days_t = setscalar_Cint32(days)
-        ticks_c, ticks_t = setscalar_Cint32(ticks)
+        year_c, year_t = setscalar_Cint32(year)
+        month_c, month_t = setscalar_Cint32(month)
+        day_c, day_t = setscalar_Cint32(day)
+        hour_c, hour_t = setscalar_Cint32(hour)
+        minute_c, minute_t = setscalar_Cint32(minute)
+        second_c, second_t = setscalar_Cint32(second)
+        tick_c, tick_t = setscalar_Cint32(tick)
+        err_msg_c, err_msg_t = set_Cchar(err_msg)
 
         _cfms_diag_set_field_init_time.argtypes = [
-            seconds_t,
-            days_t,
-            ticks_t,
+            year_t,
+            month_t,
+            day_t,
+            hour_t,
+            minute_t,
+            second_t,
+            tick_t,
+            err_msg_t,
         ]
         _cfms_diag_set_field_init_time.restype = None
 
         _cfms_diag_set_field_init_time(
-            seconds_c,
-            days_t,
-            ticks_t
+            year_c,
+            month_c,
+            day_c,
+            hour_c,
+            minute_c,
+            second_c,
+            tick_c,
+            err_msg_c
         )
+
+        return err_msg_c.value.decode('utf-8')
+    
+    def diag_set_field_timestep(
+            self,
+            diag_field_id: int,
+            dseconds: int,
+            ddays: Optional[int],
+            dticks: Optional[int],
+    ) -> str:
+        
+        err_msg = ""
+
+        _cfms_diag_set_field_timestep = self.clibFMS.cFMS_diag_set_field_timestep
+
+        diag_field_id_c, diag_field_id_t = setscalar_Cint32(diag_field_id)
+        dseconds_c, dseconds_t = setscalar_Cint32(dseconds)
+        ddays_c, ddays_t = setscalar_Cint32(ddays)
+        dticks_c, dticks_t = setscalar_Cint32(dticks)
+        err_msg_c, err_msg_t = set_Cchar(err_msg)
+
+        _cfms_diag_set_field_timestep.argtypes = [
+            diag_field_id_t,
+            dseconds_t,
+            ddays_t,
+            dticks_t,
+            err_msg_t,
+        ]
+        _cfms_diag_set_field_timestep.restype = None
+
+        _cfms_diag_set_field_timestep(
+            diag_field_id_c,
+            dseconds_c,
+            ddays_c,
+            dticks_c,
+            err_msg_c
+        )
+
+        return err_msg_c.value.decode('utf-8')
+    
+    def diag_advance_field_time(
+            self,
+            diag_field_id: int,
+    ):
+        _cfms_diag_advance_field_time = self.clibFMS.cFMS_diag_advance_field_time
+
+        diag_field_id_c, diag_field_id_t = setscalar_Cint32(diag_field_id)
+
+        _cfms_diag_advance_field_time.argtypes = [diag_field_id_t]
+        _cfms_diag_advance_field_time.restype = None
+
+        _cfms_diag_advance_field_time(diag_field_id_c)
+
+    def diag_set_time_end(
+            self,
+            year: Optional[int],
+            month: Optional[int],
+            day: Optional[int],
+            hour: Optional[int],
+            minute: Optional[int],
+            second: Optional[int],
+            tick: Optional[int],
+            err_msg: Optional[str],
+    ):
+        _cfms_set_time_end = self.clibFMS.diag_set_time_end
+
+        year_c, year_t = setscalar_Cint32(year)
+        month_c, month_t = setscalar_Cint32(month)
+        day_c, day_t = setscalar_Cint32(day)
+        hour_c, hour_t = setscalar_Cint32(hour)
+        minute_c, minute_t = setscalar_Cint32(minute)
+        second_c, second_t = setscalar_Cint32(second)
+        tick_c, tick_t = setscalar_Cint32(tick)
+        err_msg_c, err_msg_t = set_Cchar(err_msg)
+
+        _cfms_set_time_end.argtypes = [
+            year_t,
+            month_t,
+            day_t,
+            hour_t,
+            minute_t,
+            second_t,
+            tick_t,
+            err_msg_t,
+        ]
+        _cfms_set_time_end.restype = None
+
+        _cfms_set_time_end(
+            year_c,
+            month_c,
+            day_c,
+            hour_c,
+            minute_c,
+            second_c,
+            tick_c,
+            err_msg_c,
+        )
+
+    
