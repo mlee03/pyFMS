767792da30d7499e78d4f8c0cd0851980cab6fe3
Frank Malatino <frank.malatino@noaa.gov>
2025-03-18 12:39:35 -0400

    Adding pre-lim version of diag_manager test

--- pyfms/__init__.py
+++ pyfms/__init__.py
@@ -1,3 +1,7 @@
+from .diag_manager.pyfms_diag_axis_init import pyFMS_diag_axis_init
+from .diag_manager.pyfms_diag_manager import pyFMS_diag_manager
+from .diag_manager.pyfms_register_diag_field import pyFMS_register_diag_field
+from .diag_manager.pyfms_send_data import pyFMS_send_data
 from .horiz_interp import HorizInterp
 from .mpp.pyfms_mpp import pyFMS_mpp
 from .mpp.pyfms_mpp_domains import (
