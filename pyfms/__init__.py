de285505b94b401c46ac9a2be39151883a4b7038
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2025-03-25 08:54:18 -0400

    name changes

--- pyfms/__init__.py
+++ pyfms/__init__.py
@@ -1,9 +1,9 @@
-from .mpp.pyfms_mpp import pyFMS_mpp
-from .mpp.pyfms_mpp_domains import (
+from .py_mpp.py_mpp import pyFMS_mpp
+from .py_mpp.py_mpp_domains import (
     pyDomain,
     pyDomainData,
     pyFMS_mpp_domains,
     pyNestDomain,
 )
-from .pyfms_field_manager import FieldTable
-from .pyfms_fms import pyFMS
+from .py_field_manager.py_field_manager import FieldTable
+from .pyfms import pyFMS
