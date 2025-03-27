f47a7eb9ba7b4e7363c5c4e7b4d8ae4c9f2d7ad9
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2025-03-27 14:56:55 -0400

    2d test in progress

--- pyfms/py_data_override/py_data_override.py
+++ pyfms/py_data_override/py_data_override.py
@@ -1,5 +1,6 @@
 import ctypes
 from typing import Any
+import numpy.typing as npt
 
 import numpy as np
 
@@ -147,3 +148,58 @@ class pyDataOverride:
 
         # TODO:  add check for override
         return data_c.value
+
+    def data_override_2d(self,
+                         gridname: str,
+                         fieldname: str,
+                         data_shape: list[int],
+                         data_type: Any, 
+                         is_in: int | None,
+                         ie_in: int | None,
+                         js_in: int | None,
+                         je_in: int | None) -> npt.NDArray:
+        
+        _data_override_2d_cfloat = self.cfms.cFMS_data_override_2d_cfloat
+        
+        gridname_t = ctypes.c_char_p
+        fieldname_t = ctypes.c_char_p
+        data_shape_t = np.ctypeslib.ndpointer(dtype=np.int32, ndim=(1), shape=(2))
+        data_t = np.ctypeslib.ndpointer(dtype=data_type, ndim=(2), shape=data_shape)
+        override_t = ctypes.c_bool        
+        is_in_t = ctypes.c_int
+        ie_in_t = ctypes.c_int
+        js_in_t = ctypes.c_int
+        je_in_t = ctypes.c_int
+
+        gridname_c = gridname_t(gridname.encode("utf-8"))
+        fieldname_c = fieldname_t(fieldname.encode("utf-8"))
+        data_shape_c = np.array(data_shape, dtype=np.int32)
+        data = np.ascontinguousarray(np.zeroes(data_shape, dtype=data_type, order="C"))
+        override = override_t(False)
+        is_in_c = is_in_t(is_in) if is_in is not None else None
+        js_in_c = js_in_t(js_in) if js_in is not None else None
+        ie_in_c = ie_in_t(ie_in) if ie_in is not None else None
+        je_in_c = je_in_t(je_in) if je_in is not None else None
+
+        
+        _data_override_2d_cfloat.restype = None
+        _data_override_2d_cfloat.argtypes = [gridname_t,
+                                             fieldname_t,
+                                             data_shape_t,
+                                             data_t,
+                                             ctypes.POINTER(override_t),
+                                             ctypes.POINTER(is_in_t),
+                                             ctypes.POINTER(ie_in_t),
+                                             ctypes.POINTER(js_in_t),
+                                             ctypes.POINTER(je_in_t)]
+        _data_override_2d_cfloat(gridname_c,
+                                 fieldname_c,
+                                 data_shape_c,
+                                 data,
+                                 override,
+                                 is_in_c,
+                                 js_in_c,
+                                 ie_in_c,
+                                 je_in_c)
+
+        return data, override.value
