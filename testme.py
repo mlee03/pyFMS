f14c7babb31835edf8f4258ecb1c32fb5a5898b8
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2025-04-08 12:40:55 -0400

    add one more thing to testme

--- testme.py
+++ testme.py
@@ -6,4 +6,9 @@ cfms_path_in_do, cfms_in_do = pyfms.pyDataOverride.getlib()
 assert(cfms_path == cfms_path_in_do)
 assert(id(cfms)==id(cfms_in_do))
 
+fake_path = "None"
+pyfms.cFMS.cfms_path = fake_path
+assert(pyfms.cFMS.getlib()[0] is not fake_path)
+
 pyfms.pyDataOverride.init()
+
