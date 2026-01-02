eed066faf6dc1796e297f5040663331bea9d6b42
mlee03 <Mikyung.Lee@lscamd50-d.gfdl.noaa.gov>
2026-01-02 12:53:18 -0500

    remove x_is_global_g

--- pyfms/py_mpp/domain.py
+++ pyfms/py_mpp/domain.py
@@ -92,7 +92,6 @@ class Domain:
             (isg, jsg) = ({self.isg}, {self.jsg})
             (ieg, jeg) = ({self.ieg}, {self.jeg})
             (xsize_g, ysize_g) = ({self.xsize_g}, {self.ysize_g})
-            (x_is_global_g, y_is_global_g): ({self.x_is_global_g}, {self.y_is_global_g})
         """
 
         return repr_str
